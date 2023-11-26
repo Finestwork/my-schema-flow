f6ed8f9a3e74270f3676509456602110f53b7645
Finestwork <Younes.Espiritu@outlook.com>
2023-11-26 21:47:15 +0800

    Add tailwindcss prettier config

--- src/renderer/src/components/TableInfo.vue
+++ src/renderer/src/components/TableInfo.vue
@@ -2,7 +2,7 @@
 import TableInfoSettings from '@components/TableInfoSettings.vue';
 </script>
 <template>
-    <div class="dark:bg-dark-800 bg-white h-full w-[25%] border-l-2 border-dark-600">
+    <div class="h-full w-[25%] border-l-2 border-dark-600 bg-white dark:bg-dark-800">
         <TableInfoSettings />
     </div>
 </template>
--- src/renderer/src/components/TableInfoSettings.vue
+++ src/renderer/src/components/TableInfoSettings.vue
@@ -2,20 +2,20 @@
 <template>
     <div>
         <button
-            class="text-xs block w-full text-left dark:text-dark-300 font-semibold px-2 py-2.5 dark:bg-dark-700 dark:hover:bg-dark-700/70"
+            class="block w-full px-2 py-2.5 text-left text-xs font-semibold dark:bg-dark-700 dark:text-dark-300 dark:hover:bg-dark-700/70"
             type="button"
         >
             <span>Table Settings</span>
         </button>
 
-        <div class="my-2.5 mx-2">
+        <div class="mx-2 my-2.5">
             <label
-                class="cursor-pointer dark:text-slate-300 text-sm font-medium inline-flex mb-2"
+                class="mb-2 inline-flex cursor-pointer text-sm font-medium dark:text-slate-300"
                 for="tableSettingsTableName"
                 >Table Name:</label
             >
             <input
-                class="block bg-transparent border-2 dark:border-slate-300 dark:text-slate-300 w-full rounded-sm px-1.5 py-1.5 font-semibold text-xs outline-none focus:ring-4 transition-shadow duration-300 focus:ring-blue-500/60 dark:focus:border-blue-300"
+                class="block w-full rounded-sm border-2 bg-transparent px-1.5 py-1.5 text-xs font-semibold outline-none transition-shadow duration-300 focus:ring-4 focus:ring-blue-500/60 dark:border-slate-300 dark:text-slate-300 dark:focus:border-blue-300"
                 type="text"
                 placeholder="Place table name here"
                 id="tableSettingsTableName"
