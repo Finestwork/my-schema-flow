d9832fce24f50d8b5efbd9c1e37f6e40863cb38c
Finestwork <Younes.Espiritu@outlook.com>
2023-11-26 08:21:47 +0800

    Add prettier rules

--- src/renderer/src/components/Canvas.vue
+++ src/renderer/src/components/Canvas.vue
@@ -1,12 +1,10 @@
-<script setup lang='ts'>
-import {VueFlow} from "@vue-flow/core";
-import {useTableStore} from "@stores/TableStore";
+<script setup lang="ts">
+import { VueFlow } from '@vue-flow/core';
+import { useTableStore } from '@stores/TableStore';
 
 const tableStore = useTableStore();
 </script>
 
 <template>
-  <VueFlow v-model="tableStore.elements">
-
-  </VueFlow>
+  <VueFlow v-model="tableStore.elements"> </VueFlow>
 </template>
