760f66033fe44da2178f1551d2566c109c7ddbfc
Finestwork <Younes.Espiritu@outlook.com>
2023-11-26 21:53:56 +0800

    Add inter and jetbrains mono as font family

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import './assets/css/fonts.css';
 import './assets/css/tailwind.css';
 import '@vue-flow/core/dist/style.css';
 import '@vue-flow/minimap/dist/style.css';
