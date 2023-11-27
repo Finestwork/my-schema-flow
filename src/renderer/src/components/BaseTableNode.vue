e9db386f630e7f97457e81a91a7543f167ab32fd
Finestwork <Younes.Espiritu@outlook.com>
2023-11-27 18:50:34 +0800

    Retouch custom node UI

--- src/renderer/src/components/BaseTableNode.vue
+++ src/renderer/src/components/BaseTableNode.vue
@@ -16,16 +16,16 @@ const Props = defineProps({
         class="relative min-h-[150px] w-[250px] overflow-hidden rounded-lg border-2 border-blue-500 bg-white pt-1.5 font-jetbrains"
     >
         <span class="absolute left-0 top-0 block h-[6px] w-full bg-blue-500"> </span>
-        <span class="block bg-blue-50 py-1 text-center text-sm font-semibold text-blue-500">{{
+        <span class="block bg-blue-50 py-2 text-center text-sm font-bold text-blue-500">{{
             Props.data.table.name
         }}</span>
         <div
-            class="flex justify-between px-2 py-2 text-sm font-medium"
+            class="flex justify-between px-2 py-2 text-xs font-medium"
             v-for="column in Props.data.table.columns"
         >
-            <span class="w-full grow truncate text-slate-500">{{ column.name }}</span>
-            <span class="w-full grow truncate text-center text-slate-500">{{ column.type }}</span>
-            <span class="w-full grow truncate text-center text-rose-500">{{
+            <span class="w-6/12 grow truncate text-slate-500">{{ column.name }}</span>
+            <span class="w-3/12 grow truncate text-center text-slate-500">{{ column.type }}</span>
+            <span class="w-2/12 grow truncate text-center text-rose-500">{{
                 column.keyConstraint ?? ''
             }}</span>
         </div>
