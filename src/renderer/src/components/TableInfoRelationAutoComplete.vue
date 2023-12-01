5359ce0f7606f3e22ba4dd2ca6a1cce2b8cb208e
Finestwork <Younes.Espiritu@outlook.com>
2023-12-01 09:26:03 +0800

    Make autocomplete a reusable component

---
+++ src/renderer/src/components/TableInfoRelationAutoComplete.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import BaseFormAutoComplete from '@components/BaseFormAutoComplete.vue';
+import BaseFormAutoCompleteItem from '@components/BaseFormAutoCompleteItem.vue';
+import { reactive } from 'vue';
+
+const states = reactive({
+    showAttributeDropdown: false,
+});
+const { id, placeholder, items } = defineProps<{
+    id: string;
+    placeholder: string;
+    items: string[];
+}>();
+const { modelValue, searchTerm } = defineModels<{
+    modelValue: string;
+    searchTerm: string;
+}>();
+const onSelectUpdateAttribute = ({ currentIndex }) => {
+    modelValue.value = items[currentIndex];
+};
+const onInputUpdateSearchTerm = (e: KeyboardEvent) => {
+    const Target = <HTMLInputElement>e.currentTarget;
+    searchTerm.value = Target.value;
+};
+const onKeydownChooseAttribute = (e: KeyboardEvent, value: string) => {
+    if (e.key.toLowerCase() !== 'enter') return;
+    modelValue.value = value;
+    searchTerm.value = value;
+    states.showAttributeDropdown = false;
+};
+const onClickChooseAttribute = (value: string) => {
+    modelValue.value = value;
+    searchTerm.value = value;
+    states.showAttributeDropdown = false;
+};
+</script>
+
+<template>
+    <BaseFormAutoComplete
+        :id="id"
+        :placeholder="placeholder"
+        v-model="modelValue"
+        v-model:show-dropdown="states.showAttributeDropdown"
+        :items="items"
+        @on-select="onSelectUpdateAttribute"
+        @input="onInputUpdateSearchTerm"
+    >
+        <template #label>
+            <slot name="label"></slot>
+        </template>
+        <template #float>
+            <div class="scrollbar-slim">
+                <BaseFormAutoCompleteItem
+                    v-for="attr in items"
+                    @keydown="onKeydownChooseAttribute($event, attr)"
+                    @click="onClickChooseAttribute(attr)"
+                >
+                    {{ attr }}
+                </BaseFormAutoCompleteItem>
+            </div>
+        </template>
+    </BaseFormAutoComplete>
+</template>
--- src/renderer/src/components/TableInfoRelationCreate.vue
+++ src/renderer/src/components/TableInfoRelationCreate.vue
@@ -1,69 +1,30 @@
 <script setup lang="ts">
-import BaseFormAutoComplete from '@components/BaseFormAutoComplete.vue';
-import BaseFormAutoCompleteItem from '@components/BaseFormAutoCompleteItem.vue';
+import TableInfoRelationAutoComplete from '@components/TableInfoRelationAutoComplete.vue';
 import { useTableStore } from '@stores/TableStore';
-import { computed, reactive, watch } from 'vue';
+import { computed, reactive } from 'vue';
 
 const tableStore = useTableStore();
 const states = reactive({
     attribute: '',
     attributeSearchTerm: '',
-    showAttributeDropdown: false,
 });
 const getAttributes = computed(() =>
     tableStore.getAttributesOfCurrentActiveNode.filter((attr) =>
         attr.toLowerCase().includes(states.attributeSearchTerm.toLowerCase()),
     ),
 );
-const onSelectUpdateAttribute = ({ currentIndex }) => {
-    states.attribute = tableStore.getAttributesOfCurrentActiveNode[currentIndex];
-};
-const onInputUpdateSearchTerm = (e: KeyboardEvent) => {
-    const Target = <HTMLInputElement>e.currentTarget;
-    states.attributeSearchTerm = Target.value;
-};
-const onKeydownChooseAttribute = (e: KeyboardEvent, value: string) => {
-    if (e.key.toLowerCase() !== 'enter') return;
-    states.attribute = value;
-    states.attributeSearchTerm = value;
-    states.showAttributeDropdown = false;
-};
-const onClickChooseAttribute = (value: string) => {
-    states.attribute = value;
-    states.attributeSearchTerm = value;
-    states.showAttributeDropdown = false;
-};
 </script>
 
 <template>
     <div>
-        <BaseFormAutoComplete
+        <TableInfoRelationAutoComplete
             id="tableInfo"
             placeholder="Place table's attribute here"
             v-model="states.attribute"
-            v-model:show-dropdown="states.showAttributeDropdown"
+            v-model:search-term="states.attributeSearchTerm"
             :items="getAttributes"
-            @on-select="onSelectUpdateAttribute"
-            @input="onInputUpdateSearchTerm"
         >
-            <template #label>Attribute:</template>
-            <template #float>
-                <div class="scrollbar-slim">
-                    <BaseFormAutoCompleteItem
-                        v-for="attr in getAttributes"
-                        @keydown="onKeydownChooseAttribute($event, attr)"
-                        @click="onClickChooseAttribute(attr)"
-                    >
-                        {{ attr }}
-                    </BaseFormAutoCompleteItem>
-                </div>
-            </template>
-        </BaseFormAutoComplete>
-        <!--        <TableInfoTextInput id="" placeholder="" v-model="states.references">-->
-        <!--            <template #label>Referenced Table:</template>-->
-        <!--        </TableInfoTextInput>-->
-        <!--        <TableInfoTextInput id="" placeholder="" v-model="states.references">-->
-        <!--            <template #label>Referenced table attribute:</template>-->
-        <!--        </TableInfoTextInput>-->
+            <template #label> Attribute: </template>
+        </TableInfoRelationAutoComplete>
     </div>
 </template>
