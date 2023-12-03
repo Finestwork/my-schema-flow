7baea69c8b2f7b7c25c2c08cf5aa3baaa211798d
Finestwork <Younes.Espiritu@outlook.com>
2023-12-04 00:21:34 +0800

    Change table's color scheme

--- src/renderer/src/components/BaseTableNode.vue
+++ src/renderer/src/components/BaseTableNode.vue
@@ -36,22 +36,24 @@ const getColumns = computed(() => {
 
 <template>
     <div
-        class="relative min-h-[150px] w-[250px] overflow-hidden rounded-lg border-2 border-blue-500 bg-white pt-1.5 font-jetbrains"
+        class="relative min-h-[150px] w-[250px] overflow-hidden rounded-lg border-2 font-jetbrains dark:border-slate-600 dark:bg-dark-800"
     >
         <NodeHandles />
-        <span class="absolute left-0 top-0 block h-[6px] w-full bg-blue-500"> </span>
-        <span class="block bg-blue-50 py-2 text-center text-sm font-bold text-blue-500">{{
-            Props.data.table.name
-        }}</span>
+        <span
+            class="block py-2 text-center text-sm font-bold dark:bg-dark-700 dark:text-slate-300"
+            >{{ Props.data.table.name }}</span
+        >
         <div class="py-2">
             <div
-                class="flex justify-between px-2 py-2 text-xs font-semibold"
+                class="flex justify-between px-2 py-2 text-xs font-bold"
                 v-for="column in getColumns"
                 :key="column.name"
             >
-                <span class="w-4/12 grow truncate text-slate-500">{{ column.name }}</span>
-                <span class="w-4/12 grow truncate text-left text-slate-500">{{ column.type }}</span>
-                <span class="w-2/12 grow truncate text-center text-rose-500">{{
+                <span class="mr-2 w-4/12 grow truncate dark:text-slate-500">{{ column.name }}</span>
+                <span class="w-4/12 grow truncate text-left dark:text-slate-600">{{
+                    column.type
+                }}</span>
+                <span class="w-2/12 grow truncate text-center dark:text-rose-500">{{
                     column.keyConstraint
                 }}</span>
             </div>
