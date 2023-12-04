9c79225a56136dceba1dc6c7a7e85fa222371844
Finestwork <Younes.Espiritu@outlook.com>
2023-12-04 14:18:30 +0800

    Customized title bar

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
+import Titlebar from '@components/Titlebar.vue';
 import Canvas from '@components/Canvas.vue';
 import TableInfo from '@components/TableInfo.vue';
 import { isDarkMode } from '@composables/useDarkModeDetection';
+import { nextTick, onMounted, onUnmounted, ref } from 'vue';
 
+const titleBar = ref();
+const contentWrapper = ref();
 isDarkMode();
+const updateHeight = () => {
+    const TitleBarComponent = <typeof Titlebar>titleBar.value;
+    const TitleBar = TitleBarComponent.$el;
+    Object.assign(contentWrapper.value.style, {
+        paddingTop: `${TitleBar.getBoundingClientRect().height / 16}rem`,
+    });
+};
+onMounted(async () => {
+    await nextTick();
+    updateHeight();
+    window.addEventListener('resize', updateHeight);
+});
+onUnmounted(() => {
+    window.removeEventListener('resize', updateHeight);
+});
 </script>
 
 <template>
-    <div class="flex h-screen justify-between dark:bg-dark-900">
+    <Titlebar ref="titleBar" />
+    <div ref="contentWrapper" class="flex h-screen justify-between dark:bg-dark-900">
         <Canvas />
         <TableInfo />
     </div>
