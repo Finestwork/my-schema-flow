7536862e8522a88bad76ea1f5973f5fcebdc8585
Finestwork <Younes.Espiritu@outlook.com>
2023-12-06 17:24:57 +0800

    Add autolayout button

--- src/renderer/src/Components/Toolbar/Toolbar.vue
+++ src/renderer/src/Components/Toolbar/Toolbar.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import VTableIcon from '@components/Shared/Icons/VTableIcon.vue';
+import VAutoLayoutIcon from '@components/Shared/Icons/VAutoLayoutIcon.vue';
 import BaseButtonIcon from '@components/Toolbar/Partials/BaseButtonIcon.vue';
 </script>
 <template>
     <div class="px-2 py-3 dark:bg-dark-800">
-        <BaseButtonIcon>
+        <BaseButtonIcon class="mr-2">
             <VTableIcon />
             <template #tooltip>Create Table</template>
+        </BaseButtonIcon>
+        <BaseButtonIcon>
+            <VAutoLayoutIcon />
+            <template #tooltip>Auto Layout</template>
         </BaseButtonIcon>
     </div>
 </template>
