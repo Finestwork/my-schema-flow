c2d146299bcd072bb10f2e5940cf3b1dd89b149b
Finestwork <Younes.Espiritu@outlook.com>
2023-12-12 17:10:03 +0800

    Add indicator in history panel

--- src/renderer/src/Components/AppInfo/History/History.vue
+++ src/renderer/src/Components/AppInfo/History/History.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import VPanelSectionWrapper from '@components/Shared/Layouts/VPanelSectionWrapper.vue';
 import { useHistoryStore } from '@stores/HistoryStore';
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 
 const history = useHistoryStore();
 const showForm = ref(true);
@@ -11,9 +11,15 @@ const showForm = ref(true);
         <template #label>History</template>
         <div>
             <button
-                class="mb-1 block w-full rounded p-1.5 text-left text-xs font-semibold text-slate-300 outline-none last-of-type:mb-1.5 dark:bg-dark-700 dark:hover:bg-blue-600/10 dark:hover:text-blue-500 dark:focus:bg-blue-600/10"
                 v-for="(item, ind) in history.getHistoryDisplay"
                 :key="`${item}${ind}`"
+                class="mb-1 block w-full rounded p-1.5 text-left text-xs font-semibold text-slate-300 outline-none last-of-type:mb-1.5"
+                :class="{
+                    'dark:bg-dark-700 dark:hover:bg-blue-600/10 dark:hover:text-blue-500 dark:focus:bg-blue-600/10':
+                        history.currentIndex !== ind,
+                    'dark:bg-blue-600/10 dark:bg-blue-600/10 dark:text-blue-500':
+                        history.currentIndex === ind,
+                }"
             >
                 {{ item }}
             </button>
