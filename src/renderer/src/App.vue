e8fb15b28654d4bf15733df1ea76fa84cf971726
Finestwork <Younes.Espiritu@outlook.com>
2023-12-13 21:05:55 +0800

    Add panel

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -5,5 +5,10 @@ import TitleBar from '@components/modules/TitleBar/TitleBar.vue';
 <template>
     <div class="dark text-black">
         <TitleBar />
+        <div class="flex h-screen w-full dark:bg-dark-900">
+            <div class="w-full max-w-[250px] dark:bg-dark-850"></div>
+            <div class="w-full"></div>
+            <div class="w-full max-w-[250px] dark:bg-dark-850"></div>
+        </div>
     </div>
 </template>
