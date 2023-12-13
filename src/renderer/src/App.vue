4e1f5c3c10498fe3a477bef49a245fe883e34094
Finestwork <Younes.Espiritu@outlook.com>
2023-12-13 15:08:20 +0800

    Clean files

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,55 +1,7 @@
-<script setup lang="ts">
-import Canvas from '@components/Canvas/Canvas.vue';
-import TableInfo from '@components/TableInfo/TableInfo.vue';
-import AppInfo from '@components/AppInfo/AppInfo.vue';
-import TitleBar from '@components/TitleBar/TitleBar.vue';
-import Toolbar from '@components/Toolbar/Toolbar.vue';
-import { useAutoLayout } from '@composables/useAutoLayout';
-import { useIPCListeners } from '@composables/useIPCListeners';
-import { nextTick, onMounted, onUnmounted, ref } from 'vue';
-
-const titleBar = ref();
-const contentWrapper = ref();
-const isCreatingTable = ref(false);
-const { runAutoLayout } = useAutoLayout();
-const { onFileSave, onFileOpened } = useIPCListeners();
-
-const updateHeight = () => {
-    const TitleBarComponent = <typeof TitleBar>titleBar.value;
-    const TitleBar = TitleBarComponent.$el;
-    Object.assign(contentWrapper.value.style, {
-        paddingTop: `${TitleBar.getBoundingClientRect().height / 16}rem`,
-    });
-};
-onMounted(async () => {
-    await nextTick();
-    updateHeight();
-    onFileSave();
-    onFileOpened();
-    document.documentElement.classList.add('dark');
-    window.addEventListener('resize', updateHeight);
-});
-onUnmounted(() => {
-    window.removeEventListener('resize', updateHeight);
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <TitleBar ref="titleBar" />
-    <div ref="contentWrapper">
-        <div class="flex h-[calc(100vh-40px)] justify-between dark:bg-dark-900">
-            <AppInfo />
-            <div class="h-full w-full">
-                <Toolbar
-                    @create-table="isCreatingTable = true"
-                    @change-layout-orientation="runAutoLayout"
-                />
-                <Canvas
-                    v-model:is-creating-table="isCreatingTable"
-                    class="h-[calc(100vh-49.4px-40px)] w-full"
-                />
-            </div>
-            <TableInfo />
-        </div>
+    <div class="text-black">
+        I am vue now
     </div>
 </template>
--- src/renderer/src/Components/TableInfo/Shared/FormAutoCompleteItem.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <button
-        class="flex w-full truncate px-2 py-2 text-xs font-semibold outline-none group-hover:text-blue-500 group-focus:text-blue-500 dark:text-slate-300 dark:hover:bg-dark-800/70 dark:hover:text-blue-500 dark:focus:bg-dark-800/70 dark:focus:text-blue-500"
-        type="button"
-    >
-        <slot></slot>
-    </button>
-</template>
