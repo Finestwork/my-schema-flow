561ef8c80f072869a5068cd3a10d4ad9baa07efa
Finestwork <Younes.Espiritu@outlook.com>
2023-12-15 06:46:42 +0800

    Add text input

---
+++ src/renderer/src/components/Base/Forms/VPanelTextInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+export type TProps = {
+    placeholder: string;
+    id: string;
+};
+
+const props = defineProps<TProps>();
+const { modelValue } = defineModels<{
+    modelValue: string;
+}>();
+</script>
+<template>
+    <div class="w-full">
+        <label
+            class="mb-1 cursor-pointer text-xs font-semibold dark:text-slate-300"
+            :for="id"
+        >
+            <slot name="label"></slot>
+        </label>
+        <input
+            :id="id"
+            v-model="modelValue"
+            class="block w-full rounded border-2 p-1.5 text-xs font-semibold outline-none transition-shadow duration-150 ease-in-out focus:ring-4 focus:ring-cyan-500/30 dark:border-slate-700 dark:bg-dark-900 dark:text-slate-500 dark:hover:border-cyan-700 hover:dark:text-slate-300 dark:focus:border-cyan-500 focus:dark:text-slate-300"
+            type="text"
+            :placeholder="placeholder"
+        />
+    </div>
+</template>
--- src/renderer/src/components/Modules/TableInformation/TableInformation.vue
+++ src/renderer/src/components/Modules/TableInformation/TableInformation.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import VPanelSectionWrapper from '@components/Base/Layouts/VPanelSectionWrapper.vue';
+import VPanelTextInput from '@components/Base/Forms/VPanelTextInput.vue';
 import BaseColumnButton from '@components/Modules/TableInformation/Partials/BaseColumnButton.vue';
 import { useCanvasStore } from '@stores/CanvasStore';
 import { computed } from 'vue';
@@ -24,7 +25,15 @@ const getColumns = computed((): Array<TColumnList> => {
         <template #label>Table Information</template>
         <template #content>
             <div class="mt-2.5">
-                <div>
+                <div v-if="canvasStore.hasActiveNode">
+                    <VPanelTextInput
+                        v-model="canvasStore.currentActiveNode.data.table.name"
+                        class="mb-4"
+                        id="columnTextInput"
+                        placeholder="Place table's name here"
+                    >
+                        <template #label> Table's Name:</template>
+                    </VPanelTextInput>
                     <BaseColumnButton
                         v-for="(column, ind) in getColumns"
                         :key="`${column.name}${ind}`"
