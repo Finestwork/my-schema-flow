a40e1954a974e11e1e9b26381517bcb8f9b867b5
Finestwork <Younes.Espiritu@outlook.com>
2023-12-15 20:40:04 +0800

    Add column length form

--- src/renderer/src/components/Modules/TableInformation/Partials/AddFormContent.vue
+++ src/renderer/src/components/Modules/TableInformation/Partials/AddFormContent.vue
@@ -2,6 +2,7 @@
 import PanelBackButton from '@components/Shared/Buttons/PanelBackButton.vue';
 import PanelFormColumnName from '@components/Shared/Forms/PanelFormColumnName.vue';
 import PanelColumnTypeForm from '@components/Shared/Forms/PanelColumnTypeForm.vue';
+import PanelFormColumnLength from '@components/Shared/Forms/PanelFormColumnLength.vue';
 import { ref } from 'vue';
 
 const { displayForm } = defineModels<{
@@ -9,13 +10,15 @@ const { displayForm } = defineModels<{
 }>();
 const columnName = ref('');
 const columnType = ref('');
+const columnLength = ref('');
 </script>
 <template>
     <div class="mt-3">
         <PanelBackButton @click="displayForm = false" />
         <div class="mt-4">
             <PanelFormColumnName v-model="columnName" class="mb-2" />
-            <PanelColumnTypeForm v-model="columnType" />
+            <PanelColumnTypeForm v-model="columnType" class="mb-2" />
+            <PanelFormColumnLength v-model="columnLength" />
         </div>
     </div>
 </template>
