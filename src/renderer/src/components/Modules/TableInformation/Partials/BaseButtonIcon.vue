4aeb3560accda9ed41f605a529585228531b58fa
Finestwork <Younes.Espiritu@outlook.com>
2023-12-15 09:39:29 +0800

    Add button controls

---
+++ src/renderer/src/components/Modules/TableInformation/Partials/BaseButtonIcon.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import VTooltip from '@components/Base/Layouts/VTooltip.vue';
+
+export type TProps = {
+    colorScheme?: 'primary' | 'danger';
+    disabled?: boolean;
+};
+
+const props = withDefaults(defineProps<TProps>(), {
+    colorScheme: 'primary',
+    disabled: false,
+});
+</script>
+<template>
+    <VTooltip :offset-y="10">
+        <button
+            type="button"
+            class="block h-[25px] w-[25px] rounded p-2 outline-none dark:stroke-slate-300"
+            :class="{
+                'dark:bg-dark-800 dark:stroke-slate-500 hover:dark:bg-cyan-950 hover:dark:stroke-cyan-500 focus:dark:bg-cyan-950 focus:dark:stroke-cyan-500':
+                    props.colorScheme === 'primary' && !props.disabled,
+                'dark:bg-red-500 dark:stroke-red-50 dark:hover:bg-red-500/90':
+                    props.colorScheme === 'danger' && !props.disabled,
+                'cursor-not-allowed dark:bg-dark-800/90 dark:stroke-slate-700':
+                    props.disabled,
+            }"
+        >
+            <slot></slot>
+        </button>
+        <template #tooltip>
+            <slot name="tooltip"></slot>
+        </template>
+    </VTooltip>
+</template>
--- src/renderer/src/components/Modules/TableInformation/Partials/DefaultContent.vue
+++ src/renderer/src/components/Modules/TableInformation/Partials/DefaultContent.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-import BaseColumnButton from '@components/Modules/TableInformation/Partials/BaseColumnButton.vue';
+import AddIcon from '@components/Shared/Icons/AddIcon.vue';
+import CopyIcon from '@components/Shared/Icons/CopyIcon.vue';
+import TrashIcon from '@components/Shared/Icons/TrashIcon.vue';
 import VPanelTextInput from '@components/Base/Forms/VPanelTextInput.vue';
+import BaseColumnButton from '@components/Modules/TableInformation/Partials/BaseColumnButton.vue';
+import BaseButtonIcon from '@components/Modules/TableInformation/Partials/BaseButtonIcon.vue';
 import { useCanvasStore } from '@stores/CanvasStore';
 import { computed, ref } from 'vue';
 
@@ -51,5 +55,23 @@ const onClickToggleActiveState = (e: MouseEvent, ind: number) => {
             <template #type>{{ column.type }}</template>
             <template #keyConstraint>{{ column.keyConstraint }}</template>
         </BaseColumnButton>
+
+        <div class="mt-2.5">
+            <BaseButtonIcon class="mr-1">
+                <AddIcon />
+                <template #tooltip>Add Column</template>
+            </BaseButtonIcon>
+            <BaseButtonIcon class="mr-1" :disabled="currentColumnIndex === -1">
+                <CopyIcon />
+                <template #tooltip>Copy Column</template>
+            </BaseButtonIcon>
+            <BaseButtonIcon
+                color-scheme="danger"
+                :disabled="currentColumnIndex === -1"
+            >
+                <TrashIcon />
+                <template #tooltip>Delete Column</template>
+            </BaseButtonIcon>
+        </div>
     </div>
 </template>
