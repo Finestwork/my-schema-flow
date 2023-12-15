f3c8a79c7f063c5b7d2207ded2ef537818faaf52
Finestwork <Younes.Espiritu@outlook.com>
2023-12-15 21:42:12 +0800

    Add action button component

--- src/renderer/src/components/Modules/TableInformation/Partials/AddFormContent.vue
+++ src/renderer/src/components/Modules/TableInformation/Partials/AddFormContent.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import VPanelActionButton from '@components/Base/Buttons/VPanelActionButton.vue';
+import AddIcon from '@components/Shared/Icons/AddIcon.vue';
 import PanelBackButton from '@components/Shared/Buttons/PanelBackButton.vue';
 import PanelFormColumnName from '@components/Shared/Forms/PanelFormColumnName.vue';
 import PanelColumnTypeForm from '@components/Shared/Forms/PanelColumnTypeForm.vue';
@@ -19,6 +21,12 @@ const columnLength = ref('');
             <PanelFormColumnName v-model="columnName" class="mb-2" />
             <PanelColumnTypeForm v-model="columnType" class="mb-2" />
             <PanelFormColumnLength v-model="columnLength" />
+            <VPanelActionButton class="mt-4">
+                <template #icon>
+                    <AddIcon />
+                </template>
+                <template #text>Add Column</template>
+            </VPanelActionButton>
         </div>
     </div>
 </template>
