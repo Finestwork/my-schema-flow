489c9f201550b56fb2b0b8f00a2097eec2aadd2c
Finestwork <Younes.Espiritu@outlook.com>
2023-12-20 16:22:01 +0800

    Add key

--- src/renderer/src/components/Modules/HIstory/History.vue
+++ src/renderer/src/components/Modules/HIstory/History.vue
@@ -10,7 +10,8 @@ const historyStore = useHistoryStore();
         <template #label>History</template>
         <template #content>
             <button
-                v-for="history in historyStore.getHistoryDisplay"
+                v-for="(history, ind) in historyStore.getHistoryDisplay"
+                :key="`${history}-${ind}`"
                 class="mb-2 block w-full rounded px-2 py-1.5 text-left text-[.6rem] font-bold outline-none last-of-type:mb-0 dark:bg-dark-700/40 dark:text-slate-400 hover:dark:bg-cyan-950/40 hover:dark:text-cyan-500 focus:dark:bg-cyan-950/40 focus:dark:text-cyan-500"
                 type="button"
             >
