e0cd2b08fc615a7ba1dcb2781e0881f6eb71a490
Finestwork <Younes.Espiritu@outlook.com>
2023-12-20 16:21:23 +0800

    Add bottom margin

--- src/renderer/src/components/Modules/HIstory/History.vue
+++ src/renderer/src/components/Modules/HIstory/History.vue
@@ -11,7 +11,7 @@ const historyStore = useHistoryStore();
         <template #content>
             <button
                 v-for="history in historyStore.getHistoryDisplay"
-                class="block w-full rounded px-2 py-1.5 text-left text-[.6rem] font-bold outline-none dark:bg-dark-700/40 dark:text-slate-400 hover:dark:bg-cyan-950/40 hover:dark:text-cyan-500 focus:dark:bg-cyan-950/40 focus:dark:text-cyan-500"
+                class="mb-2 block w-full rounded px-2 py-1.5 text-left text-[.6rem] font-bold outline-none last-of-type:mb-0 dark:bg-dark-700/40 dark:text-slate-400 hover:dark:bg-cyan-950/40 hover:dark:text-cyan-500 focus:dark:bg-cyan-950/40 focus:dark:text-cyan-500"
                 type="button"
             >
                 {{ history }}
