688fea1902858b0c3c64f89c6a434b307946a332
Finestwork <Younes.Espiritu@outlook.com>
2023-12-21 22:14:46 +0800

    Add history panel

--- src/renderer/src/components/Modules/HistoryPanel/HistoryPanel.vue
+++ src/renderer/src/components/Modules/HistoryPanel/HistoryPanel.vue
@@ -1,1 +1,31 @@
-<template></template>
+<script setup lang="ts">
+import VPanelSectionWrapper from '@components/Base/Wrappers/VPanelSectionWrapper.vue';
+import { useHistory } from '@composables/Miscellaneous/useHistory';
+import { useHistoryStore } from '@stores/History';
+
+const { jumpHistory } = useHistory();
+const historyStore = useHistoryStore();
+</script>
+
+<template>
+    <VPanelSectionWrapper>
+        <template #label>History</template>
+        <template #content>
+            <button
+                v-for="(history, ind) in historyStore.getHistoryDisplay"
+                :key="`${history}-${ind}`"
+                class="mb-2 block w-full rounded px-2 py-1.5 text-left text-[.6rem] font-bold outline-none last-of-type:mb-0"
+                :class="{
+                    'dark:bg-cyan-950/40 dark:text-cyan-500':
+                        ind === historyStore.currentIndex,
+                    'dark:bg-dark-700/40 dark:text-slate-400 hover:dark:bg-cyan-950/40 hover:dark:text-cyan-500 focus:dark:bg-cyan-950/40 focus:dark:text-cyan-500':
+                        ind !== historyStore.currentIndex,
+                }"
+                type="button"
+                @click="jumpHistory(ind)"
+            >
+                {{ history }}
+            </button>
+        </template>
+    </VPanelSectionWrapper>
+</template>
