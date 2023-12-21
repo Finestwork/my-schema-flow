26efb54a01f5b9613ebb1525ee25e0173e65f1d1
Finestwork <Younes.Espiritu@outlook.com>
2023-12-21 19:58:35 +0800

    Add functionality to export image

--- src/renderer/src/components/Modules/Toolbar/Partials/ExportButton.vue
+++ src/renderer/src/components/Modules/Toolbar/Partials/ExportButton.vue
@@ -1,9 +1,19 @@
-w
 <script setup lang="ts">
 import VToolbarButtonDropdown from '@components/Base/Dropdowns/VToolbarButtonDropdown.vue';
 import VToolbarButtonDropdownItem from '@components/Base/Dropdowns/VToolbarButtonDropdownItem.vue';
 import ExportImageIcon from '@components/Shared/Icons/ExportImageIcon.vue';
+import { exportAsImage } from '@utilities/ExportHelper';
+import { vueFlowKey } from '@symbols/VueFlow';
+import { inject } from 'vue';
 import type { TExportTypes } from '@utilities/ExportHelper';
+
+const { vueFlowRef, getNodes } = inject(vueFlowKey);
+const onClickExportAsImage = (type: TExportTypes) => {
+    const Wrapper: HTMLElement = vueFlowRef.value?.querySelector(
+        '.vue-flow__transformationpane',
+    );
+    exportAsImage(type, Wrapper, getNodes.value);
+};
 </script>
 
 <template>
@@ -11,13 +21,13 @@ import type { TExportTypes } from '@utilities/ExportHelper';
         <ExportImageIcon />
         <template #tooltip>Export As Image</template>
         <template #float>
-            <VToolbarButtonDropdownItem>
+            <VToolbarButtonDropdownItem @click="onClickExportAsImage('png')">
                 <template #text>Export To PNG</template>
             </VToolbarButtonDropdownItem>
-            <VToolbarButtonDropdownItem>
+            <VToolbarButtonDropdownItem @click="onClickExportAsImage('jpg')">
                 <template #text>Export To JPG</template>
             </VToolbarButtonDropdownItem>
-            <VToolbarButtonDropdownItem>
+            <VToolbarButtonDropdownItem @click="onClickExportAsImage('svg')">
                 <template #text>Export To SVG</template>
             </VToolbarButtonDropdownItem>
         </template>
