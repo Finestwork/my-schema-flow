a26cab9ae3825c0de0eb9e0a42c8b861ab0bf7f2
Finestwork <Younes.Espiritu@outlook.com>
2023-12-21 22:23:05 +0800

    Add functionality to undo/redo history

--- src/renderer/src/components/Modules/Toolbar/Partials/RedoButton.vue
+++ src/renderer/src/components/Modules/Toolbar/Partials/RedoButton.vue
@@ -2,12 +2,14 @@
 import VToolbarButtonIcon from '@components/Base/ButtonIcons/VToolbarButtonIcon.vue';
 import RedoIcon from '@components/Shared/Icons/RedoIcon.vue';
 import { useHistoryStore } from '@stores/History';
+import { useHistory } from '@composables/Miscellaneous/useHistory';
 
 const historyStore = useHistoryStore();
+const { redoHistory } = useHistory();
 </script>
 
 <template>
-    <VToolbarButtonIcon :disabled="!historyStore.canRedo">
+    <VToolbarButtonIcon :disabled="!historyStore.canRedo" @click="redoHistory">
         <RedoIcon />
         <template #tooltip>Redo</template>
     </VToolbarButtonIcon>
--- src/renderer/src/components/Modules/Toolbar/Partials/UndoButton.vue
+++ src/renderer/src/components/Modules/Toolbar/Partials/UndoButton.vue
@@ -2,12 +2,14 @@
 import VToolbarButtonIcon from '@components/Base/ButtonIcons/VToolbarButtonIcon.vue';
 import UndoIcon from '@components/Shared/Icons/UndoIcon.vue';
 import { useHistoryStore } from '@stores/History';
+import { useHistory } from '@composables/Miscellaneous/useHistory';
 
 const historyStore = useHistoryStore();
+const { undoHistory } = useHistory();
 </script>
 
 <template>
-    <VToolbarButtonIcon :disabled="!historyStore.canUndo">
+    <VToolbarButtonIcon :disabled="!historyStore.canUndo" @click="undoHistory">
         <UndoIcon />
         <template #tooltip>Undo</template>
     </VToolbarButtonIcon>
