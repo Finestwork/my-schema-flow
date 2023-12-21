91b6cd0cc743890628e6c5a96b79bb2af078ab17
Finestwork <Younes.Espiritu@outlook.com>
2023-12-21 19:40:16 +0800

    Add functionality where user can click autolayout button from the toolbar

--- src/renderer/src/components/Modules/Toolbar/Partials/AutoLayoutButton.vue
+++ src/renderer/src/components/Modules/Toolbar/Partials/AutoLayoutButton.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import VToolbarButtonIcon from '@components/Base/ButtonIcons/VToolbarButtonIcon.vue';
 import AutoLayoutIcon from '@components/Shared/Icons/AutoLayoutIcon.vue';
+import { useNodeAutoLayout } from '@composables/Nodes/useAutoLayout';
+const { autoLayout } = useNodeAutoLayout();
 </script>
 
 <template>
-    <VToolbarButtonIcon>
+    <VToolbarButtonIcon @click="autoLayout">
         <AutoLayoutIcon />
         <template #tooltip>Run AutoLayout</template>
     </VToolbarButtonIcon>
--- src/renderer/src/composables/Nodes/useAutoLayout.ts
+++ src/renderer/src/composables/Nodes/useAutoLayout.ts
@@ -1,12 +1,13 @@
 import { useSettingsStore } from '@stores/Settings';
 import { useEdgePositionCalculator } from '@composables/Edges/useEdgePositionCalculator';
 import { nodeAutolayout } from '@utilities/CanvasHelper';
-import { useVueFlow } from '@vue-flow/core';
+import { vueFlowKey } from '@symbols/VueFlow';
+import { inject } from 'vue';
 
 export function useNodeAutoLayout() {
     const settingsStore = useSettingsStore();
     const { calculateAllEdgesPosition } = useEdgePositionCalculator();
-    const { getEdges, getNodes, setNodes, setEdges } = useVueFlow();
+    const { getEdges, getNodes, setNodes, setEdges } = inject(vueFlowKey);
     const autoLayout = () => {
         const { nodes, edges } = nodeAutolayout(
             getNodes.value,
