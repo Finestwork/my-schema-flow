536c1b7f0edc7d480486eedc36384317ad8502ec
Finestwork <Younes.Espiritu@outlook.com>
2023-12-25 17:43:48 +0800

    Remove the ability of user to drag handle components

--- src/renderer/src/components/Modules/Canvas/Partials/CustomNodeHandles.vue
+++ src/renderer/src/components/Modules/Canvas/Partials/CustomNodeHandles.vue
@@ -2,13 +2,53 @@
 import { Handle, Position } from '@vue-flow/core';
 </script>
 <template>
-    <Handle id="source-top" type="source" :position="Position.Top" />
-    <Handle id="source-bottom" type="source" :position="Position.Bottom" />
-    <Handle id="source-right" type="source" :position="Position.Right" />
-    <Handle id="source-left" type="source" :position="Position.Left" />
+    <Handle
+        id="source-top"
+        type="source"
+        :position="Position.Top"
+        :connectable="false"
+    />
+    <Handle
+        id="source-bottom"
+        type="source"
+        :position="Position.Bottom"
+        :connectable="false"
+    />
+    <Handle
+        id="source-right"
+        type="source"
+        :position="Position.Right"
+        :connectable="false"
+    />
+    <Handle
+        id="source-left"
+        type="source"
+        :position="Position.Left"
+        :connectable="false"
+    />
 
-    <Handle id="target-top" type="target" :position="Position.Top" />
-    <Handle id="target-bottom" type="target" :position="Position.Bottom" />
-    <Handle id="target-right" type="target" :position="Position.Right" />
-    <Handle id="target-left" type="target" :position="Position.Left" />
+    <Handle
+        id="target-top"
+        type="target"
+        :position="Position.Top"
+        :connectable="false"
+    />
+    <Handle
+        id="target-bottom"
+        type="target"
+        :position="Position.Bottom"
+        :connectable="false"
+    />
+    <Handle
+        id="target-right"
+        type="target"
+        :position="Position.Right"
+        :connectable="false"
+    />
+    <Handle
+        id="target-left"
+        type="target"
+        :position="Position.Left"
+        :connectable="false"
+    />
 </template>
