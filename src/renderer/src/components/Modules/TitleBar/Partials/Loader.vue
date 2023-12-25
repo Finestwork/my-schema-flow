3251d2b9c49473360b1ac89e5ea9ed577f279f37
Finestwork <Younes.Espiritu@outlook.com>
2023-12-25 21:29:54 +0800

    Add loader

---
+++ src/renderer/src/components/Modules/TitleBar/Partials/Loader.vue
@@ -0,0 +1,5 @@
+<template>
+    <p class="text-xs font-semibold dark:text-slate-400">
+        Saving, please don't exit.
+    </p>
+</template>
--- src/renderer/src/components/Modules/TitleBar/TitleBar.vue
+++ src/renderer/src/components/Modules/TitleBar/TitleBar.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import VPanelTextInput from '@components/Base/Forms/VPanelTextInput.vue';
+import Loader from '@components/Modules/TitleBar/Partials/Loader.vue';
 import { useFileStore } from '@stores/File';
 import { useTrackChange } from '@composables/History/useTrackChange';
 import { useSaveCanvas } from '@composables/Canvas/useSaveCanvas';
-import { watchOnce } from '@vueuse/core';
-import { computed, ref } from 'vue';
+import { computed, nextTick, onMounted, ref } from 'vue';
 
 const fileStore = useFileStore();
 const input = ref();
 const isEditing = ref(false);
+const isLoading = ref(false);
 const currentFileName = ref('');
 const { hasChanged } = useTrackChange();
 const { saveCanvas, overwriteFileName } = useSaveCanvas();
@@ -27,48 +28,59 @@ const onEnterSaveFile = () => {
         return;
     }
 
+    isLoading.value = true;
     overwriteFileName(currentFileName.value);
 };
 const onBlurResetForm = () => {
     currentFileName.value = fileStore.getFileNameWithoutExt;
     isEditing.value = false;
 };
-watchOnce(
-    () => fileStore.fileName,
-    () => {
+onMounted(() => {
+    window.electron.ipcRenderer.on('fileNameOverwriteSuccessfully', () => {
+        // Add timeout to avoid content jumping
+        setTimeout(() => {
+            currentFileName.value = fileStore.getFileNameWithoutExt;
+            isLoading.value = false;
+        }, 350);
+    });
+    window.electron.ipcRenderer.on('fileSavedSuccessfully', async () => {
+        await nextTick();
         currentFileName.value = fileStore.getFileNameWithoutExt;
-    },
-);
+    });
+});
 </script>
 
 <template>
     <div
         class="drag-title-bar relative z-[500] flex h-[40px] select-none items-center px-2 dark:bg-dark-950"
     >
-        <div class="no-drag-title-bar h-full py-2">
+        <div class="no-drag-title-bar flex h-full items-center py-2">
             <span
                 v-if="fileStore.filePath === ''"
                 class="text-xs font-semibold dark:text-slate-500"
                 >{{ defaultFileName }}</span
             >
             <template v-else>
-                <button
-                    v-if="!isEditing"
-                    class="h-full rounded px-2 text-xs font-semibold outline-none dark:text-slate-500 hover:dark:bg-cyan-950/30 hover:dark:text-cyan-500 focus:dark:text-cyan-500"
-                    type="button"
-                    @dblclick="isEditing = true"
-                >
-                    {{ defaultFileName }}
-                </button>
-                <VPanelTextInput
-                    v-else
-                    id="titleBarFileName"
-                    ref="input"
-                    v-model="currentFileName"
-                    placeholder="Place file name here"
-                    @keydown.enter="onEnterSaveFile"
-                    @blur="onBlurResetForm"
-                />
+                <template v-if="!isLoading">
+                    <button
+                        v-if="!isEditing"
+                        class="h-full rounded px-2 text-xs font-semibold outline-none dark:text-slate-500 hover:dark:bg-cyan-950/30 hover:dark:text-cyan-500 focus:dark:text-cyan-500"
+                        type="button"
+                        @dblclick="isEditing = true"
+                    >
+                        {{ defaultFileName }}
+                    </button>
+                    <VPanelTextInput
+                        v-else
+                        id="titleBarFileName"
+                        ref="input"
+                        v-model="currentFileName"
+                        placeholder="Place file name here"
+                        @keydown.enter="onEnterSaveFile"
+                        @blur="onBlurResetForm"
+                    />
+                </template>
+                <Loader v-else />
             </template>
         </div>
     </div>
