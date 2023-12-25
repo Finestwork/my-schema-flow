a47ab721b18f06a11cbea4ea2c329ddd0deda873
Finestwork <Younes.Espiritu@outlook.com>
2023-12-25 18:57:01 +0800

    Fix table's name UI display

--- src/renderer/src/components/Modules/Canvas/Partials/CustomNode.vue
+++ src/renderer/src/components/Modules/Canvas/Partials/CustomNode.vue
@@ -44,7 +44,7 @@ onMounted(() => {
     >
         <CustomNodeHandles />
         <span
-            class="block py-2 text-center text-sm font-bold"
+            class="block truncate px-1 py-2 text-center text-sm font-bold"
             :class="{
                 'dark:bg-cyan-950/30 dark:text-cyan-500': isActive,
                 'dark:bg-dark-800 dark:text-dark-100': isDefault,
