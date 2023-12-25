9ee0b832f2d0ce652a0a2d54883b6545829360d2
Finestwork <Younes.Espiritu@outlook.com>
2023-12-26 00:18:54 +0800

    Add light mode to custom nodes

--- src/renderer/src/components/Modules/Canvas/Partials/CustomNode.vue
+++ src/renderer/src/components/Modules/Canvas/Partials/CustomNode.vue
@@ -35,20 +35,23 @@ onMounted(() => {
 <template>
     <div
         ref="root"
-        class="relative min-h-[150px] w-[350px] overflow-hidden rounded-lg border-2 font-neon-mono hover:cursor-pointer"
+        class="relative min-h-[150px] w-[350px] overflow-hidden rounded-lg border-2 bg-white font-neon-mono hover:cursor-pointer dark:bg-dark-900"
         :class="{
-            'dark:border-cyan-600 dark:bg-dark-900': isActive,
-            'dark:border-dark-700 dark:bg-dark-900': isDefault,
-            'dark:border-dark-800 dark:bg-dark-900': isFaded,
+            'border-cyan-500 dark:border-cyan-600': isActive,
+            'border-slate-600 dark:border-dark-700': isDefault,
+            'border-slate-100 dark:border-dark-800': isFaded,
         }"
     >
         <CustomNodeHandles />
         <span
             class="block truncate px-1 py-2 text-center text-sm font-bold"
             :class="{
-                'dark:bg-cyan-950/30 dark:text-cyan-500': isActive,
-                'dark:bg-dark-800 dark:text-dark-100': isDefault,
-                'dark:bg-dark-800/40 dark:text-dark-700': isFaded,
+                'bg-cyan-500 text-cyan-50 dark:bg-cyan-950/30 dark:text-cyan-500':
+                    isActive,
+                'bg-slate-200 text-slate-800 dark:bg-dark-800 dark:text-dark-100':
+                    isDefault,
+                'bg-slate-100 text-slate-300 dark:bg-dark-800/40 dark:text-dark-700':
+                    isFaded,
             }"
             >{{ props.data.table.name }}</span
         >
@@ -61,9 +64,9 @@ onMounted(() => {
                 <span
                     class="mr-2 w-4/12 grow truncate"
                     :class="{
-                        'dark:text-slate-400':
+                        'text-slate-800 dark:text-slate-400':
                             (isActive || isDefault) && !isFaded,
-                        'dark:text-slate-700/50':
+                        'text-slate-600/40 dark:text-slate-700/50':
                             !(isActive || isDefault) && isFaded,
                     }"
                     >{{ column.name }}</span
@@ -71,9 +74,9 @@ onMounted(() => {
                 <span
                     class="w-4/12 grow truncate text-left"
                     :class="{
-                        'dark:text-slate-600':
+                        'text-slate-500 dark:text-slate-600':
                             (isActive || isDefault) && !isFaded,
-                        'dark:text-slate-700/50':
+                        'text-slate-400/50 dark:text-slate-700/50':
                             !(isActive || isDefault) && isFaded,
                     }"
                     >{{ column.type }}</span
@@ -81,9 +84,8 @@ onMounted(() => {
                 <span
                     class="w-2/12 grow truncate text-center"
                     :class="{
-                        'dark:text-cyan-500':
-                            (isActive || isDefault) && !isFaded,
-                        'dark:text-slate-700/50':
+                        'text-cyan-500': (isActive || isDefault) && !isFaded,
+                        'text-slate-700/50':
                             !(isActive || isDefault) && isFaded,
                     }"
                     >{{ column.keyConstraint }}</span
