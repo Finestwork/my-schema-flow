7624cd363cd72b2ac142fdf580aad6ea963e4045
Finestwork <Younes.Espiritu@outlook.com>
2023-12-27 00:56:01 +0800

    Fix UI bug in node's placeholder

--- src/renderer/src/components/Modules/Canvas/Partials/CustomNodePlaceholder.vue
+++ src/renderer/src/components/Modules/Canvas/Partials/CustomNodePlaceholder.vue
@@ -10,6 +10,6 @@ const { canShow } = usePlaceholder(placeholder);
     <span
         v-if="canShow"
         ref="placeholder"
-        class="font-jetbrains absolute min-h-[50px] w-[100px] overflow-hidden rounded border-2 opacity-50 hover:cursor-pointer dark:border-slate-700 dark:bg-dark-800"
+        class="font-jetbrains absolute min-h-[50px] w-[100px] overflow-hidden rounded border-2 border-dark-300 bg-white opacity-50 hover:cursor-pointer dark:border-slate-700 dark:bg-dark-800"
     ></span>
 </template>
