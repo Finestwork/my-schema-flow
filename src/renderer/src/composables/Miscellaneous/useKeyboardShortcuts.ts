e3f09d918422b287e6351db99459bfb2084408d8
Finestwork <Younes.Espiritu@outlook.com>
2023-12-28 03:50:08 +0800

    Add keyboard shortcut to toggle dark mode

--- src/renderer/src/composables/Miscellaneous/useKeyboardShortcuts.ts
+++ src/renderer/src/composables/Miscellaneous/useKeyboardShortcuts.ts
@@ -1,6 +1,7 @@
 import { useSaveCanvas } from '@composables/Canvas/useSaveCanvas';
 import { useHistoryActions } from '@composables/History/useHistoryActions';
 import { useCreateNode } from '@composables/Nodes/useCreateNode';
+import { useDarkMode } from '@composables/Miscellaneous/useDarkMode';
 import { onUnmounted } from 'vue';
 import { useVueFlow } from '@vue-flow/core';
 
@@ -8,12 +9,15 @@ export function useKeyboardShortcuts() {
     const { saveCanvas } = useSaveCanvas();
     const { redoHistory, undoHistory } = useHistoryActions();
     const { createNode } = useCreateNode();
+    const { isDarkMode, toggleDarkMode } = useDarkMode();
     const { vueFlowRef } = useVueFlow();
 
     const shortcut = (e: KeyboardEvent) => {
         const SaveKey = (e.ctrlKey || e.metaKey) && e.key === 's';
         const RedoKey = (e.ctrlKey || e.metaKey) && e.key === 'y';
         const UndoKey = (e.ctrlKey || e.metaKey) && e.key === 'z';
+        const ToggleDarkModeKey =
+            (e.ctrlKey || e.metaKey) && e.shiftKey && e.key === 'M';
         const CreateTableKey =
             (e.ctrlKey || e.metaKey) &&
             e.key.toLowerCase() === 'c' &&
@@ -29,6 +33,9 @@ export function useKeyboardShortcuts() {
             if (!vueFlowRef.value) return;
             const { left, top } = vueFlowRef.value.getBoundingClientRect();
             createNode(left + 50, top + 50);
+        } else if (ToggleDarkModeKey) {
+            toggleDarkMode();
+            window.api.toggleDarkMode(isDarkMode.value);
         }
     };
 
