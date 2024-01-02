c7dcdd24692d6641a40aed217d176ed3775cd36c
Finestwork <Younes.Espiritu@outlook.com>
2024-01-02 23:24:11 +0800

    Add switch checkbox component

--- src/renderer/src/components/Modules/Settings/Partials/Contents/AppearanceContent.vue
+++ src/renderer/src/components/Modules/Settings/Partials/Contents/AppearanceContent.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import VSwitchCheckbox from '@components/Base/Forms/VSwitchCheckbox.vue';
+import { ref } from 'vue';
+
+const checkboxEnabled = ref(false);
+</script>
 <template>
-    <div>Content</div>
+    <div>
+        <VSwitchCheckbox
+            id="toggleDarkMode"
+            v-model="checkboxEnabled"
+            for="toggleDarkMode"
+        >
+            <template #label>Toggle Dark Mode</template>
+        </VSwitchCheckbox>
+    </div>
 </template>
