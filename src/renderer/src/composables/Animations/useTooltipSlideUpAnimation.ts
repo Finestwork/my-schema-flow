70464f876545a9ddd84683a84c6d7400c7b68665
Finestwork <Younes.Espiritu@outlook.com>
2024-01-04 00:00:39 +0800

    Fix arrow position

--- src/renderer/src/composables/Animations/useTooltipSlideUpAnimation.ts
+++ src/renderer/src/composables/Animations/useTooltipSlideUpAnimation.ts
@@ -27,7 +27,7 @@ export function useTooltipSlideUpAnimation(
     const MergedOptions = Object.assign(DefaultOptions, options);
     const currentPlacement = ref(MergedOptions.placement);
 
-    const onEnter = (el: Element, done: () => void) => {
+    const onEnter = async (el: Element, done: () => void) => {
         const Middlewares = [
             offset({ mainAxis: MergedOptions?.offset?.y ?? 0 }),
             flip(),
@@ -43,83 +43,85 @@ export function useTooltipSlideUpAnimation(
                 }),
             );
         }
-        computePosition(source.value, el as HTMLElement, {
-            placement: MergedOptions.placement as Placement,
-            middleware: Middlewares,
-        }).then(({ x, y, placement, middlewareData }) => {
-            const Offset = 15;
-            const ArrowWidth = 6; // 6px
-            const InitialTop = placement === 'top' ? y - Offset : y + Offset;
-            currentPlacement.value = placement;
+        const { x, y, placement, middlewareData } = await computePosition(
+            source.value,
+            el as HTMLElement,
+            {
+                placement: MergedOptions.placement as Placement,
+                middleware: Middlewares,
+            },
+        );
 
-            // Check arrow element existence
-            if ('arrow' in MergedOptions) {
-                const ArrowMiddleware = middlewareData.arrow;
-                if (!ArrowMiddleware) return;
-                const ArrowElement = toValue(
-                    MergedOptions.arrow,
-                ) as HTMLElement;
+        const Offset = 15;
+        const ArrowWidth = 6; // 6px
+        const InitialTop = placement === 'top' ? y - Offset : y + Offset;
+        currentPlacement.value = placement;
 
-                if (placement === 'top') {
-                    Object.assign(ArrowElement.style, {
-                        left:
-                            ArrowMiddleware.x != null
-                                ? `${ArrowMiddleware.x}px`
-                                : '',
-                        bottom: '-4.5px', // Since border-width is .3rem or 6px
-                        transform: 'rotate(180deg) translateY(-50%)',
-                    });
-                }
+        // Check arrow element existence
+        if ('arrow' in MergedOptions) {
+            const ArrowMiddleware = middlewareData.arrow;
+            if (!ArrowMiddleware) return;
+            const ArrowElement = toValue(MergedOptions.arrow) as HTMLElement;
 
-                if (placement === 'bottom') {
-                    Object.assign(ArrowElement.style, {
-                        left:
-                            ArrowMiddleware.x != null
-                                ? `${ArrowMiddleware.x}px`
-                                : '',
-                        top: '-4.5px', // Since border-width is .3rem or 6px
-                        transform: 'translateY(-50%)',
-                    });
-                }
-
-                if (placement === 'right') {
-                    Object.assign(ArrowElement.style, {
-                        left: '0',
-                        top: ArrowMiddleware.y, // Since border-width is .3rem or 6px
-                        transform: 'translateX(-100%) rotate(-90deg)',
-                    });
-                }
-
-                if (placement === 'left') {
-                    Object.assign(ArrowElement.style, {
-                        right: '0',
-                        top: ArrowMiddleware.y, // Since border-width is .3rem or 6px
-                        transform: 'translateX(100%) rotate(90deg)',
-                    });
-                }
+            if (placement === 'top') {
+                Object.assign(ArrowElement.style, {
+                    left:
+                        ArrowMiddleware.x != null
+                            ? `${ArrowMiddleware.x}px`
+                            : '',
+                    bottom: '-4.5px', // Since border-width is .3rem or 6px
+                    transform: 'rotate(180deg) translateY(-50%)',
+                });
             }
 
-            Object.assign((<HTMLElement>el).style, {
-                left: `${x}px`,
-                top: `${InitialTop}px`,
-                opacity: 0,
-            });
+            if (placement === 'bottom') {
+                Object.assign(ArrowElement.style, {
+                    left:
+                        ArrowMiddleware.x != null
+                            ? `${ArrowMiddleware.x}px`
+                            : '',
+                    top: '-4.5px', // Since border-width is .3rem or 6px
+                    transform: 'translateY(-50%)',
+                });
+            }
 
-            const FinalTop =
-                'arrow' in MergedOptions
-                    ? placement !== 'top'
-                        ? y + ArrowWidth
-                        : y - ArrowWidth
-                    : y;
+            if (placement === 'right') {
+                Object.assign(ArrowElement.style, {
+                    left: '0',
+                    top: '50%',
+                    transform: 'translate(-100%, -50%) rotate(-90deg)',
+                });
+            }
 
-            anime({
-                targets: el,
-                top: FinalTop,
-                opacity: 1,
-                duration: 350,
-                easing: 'easeOutQuint',
-                complete: done,
-            });
+            if (placement === 'left') {
+                Object.assign(ArrowElement.style, {
+                    right: '0',
+                    top: '50%',
+                    transform: 'translate(100%, -50%) rotate(90deg)',
+                });
+            }
+        }
+
+        Object.assign((<HTMLElement>el).style, {
+            left: `${x}px`,
+            top: `${InitialTop}px`,
+            opacity: 0,
+        });
+
+        const FinalTop =
+            'arrow' in MergedOptions
+                ? placement !== 'top'
+                    ? y + ArrowWidth
+                    : y - ArrowWidth
+                : y;
+
+        anime({
+            targets: el,
+            top: FinalTop,
+            opacity: 1,
+            duration: 350,
+            easing: 'easeOutQuint',
+            complete: done,
         });
     };
     const onLeave = (el: Element, done: () => void) => {
