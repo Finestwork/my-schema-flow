f52c49479485afafae5a604563f9623b05616ee1
Finestwork <Younes.Espiritu@outlook.com>
2024-01-11 09:13:29 +0800

    Fix regex pattern where 'use' keyword is not being matched

--- src/renderer/src/utilities/Editor/LineValidatorHelper.ts
+++ src/renderer/src/utilities/Editor/LineValidatorHelper.ts
@@ -2,7 +2,7 @@
  * This will check if 'use' keyword has been used in the text editor
  */
 export const checkUseKeywordExistence = (code: string) => {
-    const Regex = /use\s*[A-Za-z]+\s*;?/;
+    const Regex = /use\s*[A-Za-z]*\s*;?/;
     const Lines = code.split('\n');
     return Lines.some((line) => Regex.test(line));
 };
