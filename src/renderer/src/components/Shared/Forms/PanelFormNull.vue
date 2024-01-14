3a7121c76ed7669c485c5cb83ec920d5b0236a38
Finestwork <Younes.Espiritu@outlook.com>
2024-01-14 12:16:41 +0800

    Add checkbox form

--- src/renderer/src/components/Shared/Forms/PanelFormNull.vue
+++ src/renderer/src/components/Shared/Forms/PanelFormNull.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import VPanelSelectButton from '@components/Base/Buttons/VPanelSelectButton.vue';
+import VPanelCheckboxButton from '@components/Base/Forms/VPanelCheckboxButton.vue';
 
 const { modelValue } = defineModels<{
     modelValue: boolean;
@@ -7,19 +7,7 @@ const { modelValue } = defineModels<{
 </script>
 
 <template>
-    <div>
-        <span class="mb-1.5 block text-xs font-bold dark:text-slate-300"
-            >Allow Null Values:</span
-        >
-        <div class="flex">
-            <VPanelSelectButton
-                class="mr-2 w-6/12"
-                :is-active="modelValue"
-                @click="modelValue = !modelValue"
-            >
-                Null</VPanelSelectButton
-            >
-            <span class="block w-6/12"></span>
-        </div>
-    </div>
+    <VPanelCheckboxButton id="nullColumnCheckbox" v-model="modelValue">
+        <template #label>Null</template>
+    </VPanelCheckboxButton>
 </template>
